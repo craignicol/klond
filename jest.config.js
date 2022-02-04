86650055320b4dc67ee5775d00689465629022da
Craig Nicol <craig.nicol@gmail.com>
2022-02-04 00:55:21 +0000

    Make the test pass

---
+++ jest.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/babel-jest',
+    '^.+\\.tsx?$': 'ts-jest',
+    "^.+\\.svelte$": ["svelte-jester", { preprocess: true }]
+  },
+  coverageDirectory: '<rootDir>/coverage',
+  testEnvironment: 'jsdom',
+  collectCoverageFrom: ['<rootDir>/src/**/*.{js,ts}', '!**/node_modules/**'],
+  coverageThreshold: {
+    global: {
+      branches: 70,
+      functions: 70,
+      lines: 70,
+      statements: 70,
+    },
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'svelte'],
+};
--- src/__tests__/App.test.ts
+++ src/__tests__/App.test.ts
@@ -3,6 +3,6 @@ import { render } from '@testing-library/svelte';
 import App from '../App.svelte';
 
 test('Shows heading when rendered', () => {
-  const { getByText } = render(App);
-  expect(getByText('Hello World')).toBeInTheDocument();
+  const { getByText } = render(App, {name: 'world'});
+  expect(getByText('Svelte tutorial')).toBeInTheDocument();
 });
