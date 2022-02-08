c58c497d38b3dccf26f831f290f3fba2f1042214
Craig Nicol <craig.nicol@gmail.com>
2022-02-08 00:03:04 +0000

    Fix tests

--- src/__tests__/App.test.ts
+++ src/__tests__/App.test.ts
@@ -1,8 +1,9 @@
+import { Shuffle } from '../deck';
 import '@testing-library/jest-dom';
 import { render } from '@testing-library/svelte';
 import App from '../App.svelte';
 
 test('Shows heading when rendered', () => {
-  const { getByText } = render(App, {name: 'world'});
-  expect(getByText('Svelte tutorial')).toBeInTheDocument();
+  const { getByText } = render(App, {deck: Shuffle(0)});
+  expect(getByText('Klond tutorial')).toBeInTheDocument();
 });
