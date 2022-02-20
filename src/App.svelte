8bad84e6a08814cd0e19cd346d8084d8d12d857a
Craig Nicol <craig.nicol@gmail.com>
2022-02-20 23:04:11 +0000

    Formatting

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-import { onMount } from "svelte";
-import Card from "./Card.svelte";
-import { Letter, LetterCard, Deal, Layout } from "./deck";
-import Shelf from "./Shelf.svelte";
-import {isWord, wordScore} from "./dictionary";
+	import { onMount } from "svelte";
+	import Card from "./Card.svelte";
+	import { Letter, LetterCard, Deal, Layout } from "./deck";
+	import Shelf from "./Shelf.svelte";
+	import { isWord, wordScore } from "./dictionary";
 
 	export let deck: LetterCard[];
 	let selected: LetterCard[] = [];
@@ -15,42 +15,59 @@ import {isWord, wordScore} from "./dictionary";
 	let layout: Layout = { columns: [[]], discard: [] };
 	let discardIndex: number = 0;
 	const verbose = true;
-	const genericCard: LetterCard = { letter: Letter.Q, deckPosition: -1, selected: false, used: false };
+	const genericCard: LetterCard = {
+		letter: Letter.Q,
+		deckPosition: -1,
+		selected: false,
+		used: false
+	};
 
 	let shelf: HTMLElement;
 	let touchedCard: LetterCard;
 	let sourceColumn: number;
-	let activeEvent = '';
-  let originalX = '';
-  let originalY = '';
+	let activeEvent = "";
+	let originalX = "";
+	let originalY = "";
 
-	function selectCard(card: LetterCard) { // Or unselect if selected
-		if(card === undefined) {
+	function selectCard(card: LetterCard) {
+		// Or unselect if selected
+		if (card === undefined) {
 			dragMessage = "card not found";
 			return;
 		}
-		if(deck[card.deckPosition].selected) {
+		if (deck[card.deckPosition].selected) {
 			selected = selected.filter(c => c.deckPosition !== card.deckPosition);
-			deck = deck.map((c, i) => i === card.deckPosition ? {...c, selected: false} : c);
+			deck = deck.map((c, i) =>
+				i === card.deckPosition ? { ...c, selected: false } : c
+			);
 		} else {
 			notFoundMessage = undefined;
-			selected = [...selected, {...card, selected: true}];
-			deck = deck.map((c, i) => i === card.deckPosition ? {...c, selected: true} : c);
+			selected = [...selected, { ...card, selected: true }];
+			deck = deck.map((c, i) =>
+				i === card.deckPosition ? { ...c, selected: true } : c
+			);
 		}
 	}
 
 	function checkWord() {
-		const word = selected.map(s => Letter[s.letter]).join('');
+		const word = selected.map(s => Letter[s.letter]).join("");
 		if (isWord(word)) {
 			const nextScore = wordScore(word);
 			foundWords = [...foundWords, word + " : " + nextScore.toString()];
 			score += nextScore;
 			selected = [];
 			notFound = undefined;
-			deck = deck.map(c => c.selected ? ({...c, selected: false, used: true}): c);
-			layout = {columns: layout.columns.map(col => col.filter(c => !deck[c.deckPosition].used)), discard: layout.discard.filter(d => !deck[d.deckPosition].used)};
+			deck = deck.map(c =>
+				c.selected ? { ...c, selected: false, used: true } : c
+			);
+			layout = {
+				columns: layout.columns.map(col =>
+					col.filter(c => !deck[c.deckPosition].used)
+				),
+				discard: layout.discard.filter(d => !deck[d.deckPosition].used)
+			};
 		} else {
-			deck = deck.map(c => c.selected ? ({...c, selected: false}): c);
+			deck = deck.map(c => (c.selected ? { ...c, selected: false } : c));
 			selected = [];
 			notFound = word;
 			notFoundMessage = notFound + " is not a word";
@@ -66,24 +83,33 @@ import {isWord, wordScore} from "./dictionary";
 
 	onMount(() => {
 		layout = Deal(deck);
-		shelf = document.getElementById("shelf"); 
+		shelf = document.getElementById("shelf");
 	});
 
-	const scrollIntoView = (node: HTMLElement)=> {
-		node.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
-	}
+	const scrollIntoView = (node: HTMLElement) => {
+		node.scrollIntoView({
+			behavior: "smooth",
+			block: "center",
+			inline: "center"
+		});
+	};
 
 	function startDrag(e: DragEvent, card: LetterCard, columnidx: number) {
 		e.dataTransfer.setData("card", card.deckPosition.toString());
 		e.dataTransfer.setData("source", columnidx.toString());
 		e.dataTransfer.dropEffect = "move";
-		dragMessage = "Dragging " + Letter[card.letter] + card.deckPosition.toString() + " from column " + columnidx.toString();
+		dragMessage =
+			"Dragging " +
+			Letter[card.letter] +
+			card.deckPosition.toString() +
+			" from column " +
+			columnidx.toString();
 	}
 
 	function dragDrop(e: DragEvent) {
 		dragMessage = "Dropping...";
 		// Only handling shelf for now
-		if ((e.target instanceof Element) && (e.target.id === "shelf")) {
+		if (e.target instanceof Element && e.target.id === "shelf") {
 			dragMessage = "Dropping on the correct shelf";
 			const card = deck[parseInt(e.dataTransfer.getData("card"))];
 			const source = parseInt(e.dataTransfer.getData("source"));
@@ -91,7 +117,11 @@ import {isWord, wordScore} from "./dictionary";
 			selectCard(card);
 			return false;
 		}
-		dragMessage = "Dropping on " + (e.target instanceof Element ? e.target.nodeName + "#" + e.target.id : "nothing");
+		dragMessage =
+			"Dropping on " +
+			(e.target instanceof Element
+				? e.target.nodeName + "#" + e.target.id
+				: "nothing");
 	}
 
 	function dragOver(e: DragEvent) {
@@ -105,91 +135,156 @@ import {isWord, wordScore} from "./dictionary";
 		return true;
 	}
 
-	function handleTouchStart(e: TouchEvent, card: LetterCard, columnidx: number) {
-    	dragMessage = "Touch start with card " + card.deckPosition.toString() + "-" + Letter[card.letter] + " from column " + columnidx.toString();
-			const sourceElement = e.target as HTMLElement;
-      originalX = (sourceElement.offsetLeft - 10) + "px";
-      originalY = (sourceElement.offsetTop - 10) + "px";
-      activeEvent = 'start';
-			touchedCard = card;
-			sourceColumn = columnidx;
-    }
+	function handleTouchStart(
+		e: TouchEvent,
+		card: LetterCard,
+		columnidx: number
+	) {
+		dragMessage =
+			"Touch start with card " +
+			card.deckPosition.toString() +
+			"-" +
+			Letter[card.letter] +
+			" from column " +
+			columnidx.toString();
+		const sourceElement = e.target as HTMLElement;
+		originalX = sourceElement.offsetLeft - 10 + "px";
+		originalY = sourceElement.offsetTop - 10 + "px";
+		activeEvent = "start";
+		touchedCard = card;
+		sourceColumn = columnidx;
+	}
 
-    function handleTouchMove(e: TouchEvent) {
-    	let touchLocation = e.targetTouches[0];
-    	let pageX = Math.floor((touchLocation.pageX - 50)) + "px";
-    	let pageY = Math.floor((touchLocation.pageY - 50)) + "px";
-    	dragMessage = "Touch x " + pageX + " Touch y " + pageY;
+	function handleTouchMove(e: TouchEvent) {
+		let touchLocation = e.targetTouches[0];
+		let pageX = Math.floor(touchLocation.pageX - 50) + "px";
+		let pageY = Math.floor(touchLocation.pageY - 50) + "px";
+		dragMessage = "Touch x " + pageX + " Touch y " + pageY;
+		const targetElement = e.target as HTMLElement;
+		targetElement.style.position = "absolute";
+		targetElement.style.left = pageX;
+		targetElement.style.top = pageY;
+		activeEvent = "move";
+	}
+
+	function handleTouchEnd(e: TouchEvent) {
+		e.preventDefault();
+		if (activeEvent === "move") {
 			const targetElement = e.target as HTMLElement;
-    	targetElement.style.position = "absolute";
-    	targetElement.style.left = pageX;
-    	targetElement.style.top = pageY;
-    	activeEvent = 'move';
-    }
+			let pageX = parseInt(targetElement.style.left) - 50;
+			let pageY = parseInt(targetElement.style.top) - 50;
 
-    function handleTouchEnd(e: TouchEvent) {
-    	e.preventDefault();
-    	if (activeEvent === 'move') {
-				const targetElement = e.target as HTMLElement;
-      	let pageX = (parseInt(targetElement.style.left) - 50);
-      	let pageY = (parseInt(targetElement.style.top) - 50);
+			if (
+				detectTouchEnd(
+					shelf.offsetLeft,
+					shelf.offsetTop,
+					pageX,
+					pageY,
+					shelf.offsetWidth,
+					shelf.offsetHeight
+				)
+			) {
+				selectCard(touchedCard);
+				targetElement.style.position = "initial";
+				dragMessage =
+					"You dropped " + targetElement.getAttribute("id") + " into drop zone";
+			} else {
+				targetElement.style.left = originalX;
+				targetElement.style.top = originalY;
+				dragMessage =
+					"You let card " + targetElement.getAttribute("id") + " go.";
+			}
 
-      	if (detectTouchEnd(shelf.offsetLeft, shelf.offsetTop, pageX, pageY, shelf.offsetWidth, shelf.offsetHeight)) {
-        	selectCard(touchedCard);
-        	targetElement.style.position = "initial";
-        	dragMessage = "You dropped " + targetElement
-          	.getAttribute('id') + " into drop zone";
-        } else {
-        	targetElement.style.left = originalX;
-        	targetElement.style.top = originalY;
-        	dragMessage = "You let card " + targetElement.getAttribute('id') + " go.";
-        }
-      }
-    }
+			touchedCard = undefined;
+			sourceColumn = undefined;
+		}
+	}
 
-    function detectTouchEnd(x1, y1, x2, y2, w, h) {
-    	//Very simple detection here
-    	if (Math.abs(x2 - x1) > w) 
-      	return false;
-    	if (Math.abs(y2 - y1) > h) 
-      	return false;
-    	return true;
-    }
-
+	function detectTouchEnd(x1, y1, x2, y2, w, h) {
+		//Very simple detection here
+		if (Math.abs(x2 - x1) > w) return false;
+		if (Math.abs(y2 - y1) > h) return false;
+		return true;
+	}
 </script>
 
 <main>
 	<h1>Klond</h1>
-	<p class="instructions">Double-click or drag cards to make words. Click the card back beside the 3-card spread to deal more cards.</p>
-	<p class="instructions">More points for longer words, but you'll lose points for any cards you can't make into words.</p>
+	<p class="instructions">
+		Double-click or drag cards to make words. Click the card back beside the
+		3-card spread to deal more cards.
+	</p>
+	<p class="instructions">
+		More points for longer words, but you'll lose points for any cards you can't
+		make into words.
+	</p>
 	<p class="instructions">US and UK spellings allowed.</p>
-	<p>Visit the <a href="https://craignicol.github.io/klond/#howtoplay">Klond tutorial</a> to find out more. <a href="https://github.com/dwyl/english-words">The word list is taken from Github</a></p>
-	<p class="hidden">{#each selected as l}{Letter[l.letter]}{:else}~~No selected letters~~{/each}</p>
+	<p>
+		Visit the <a href="https://craignicol.github.io/klond/#howtoplay"
+			>Klond tutorial</a
+		>
+		to find out more.
+		<a href="https://github.com/dwyl/english-words"
+			>The word list is taken from Github</a
+		>
+	</p>
+	<p class="hidden">
+		{#each selected as l}{Letter[l.letter]}{:else}~~No selected letters~~{/each}
+	</p>
 
-	<Shelf bind:currentWord={selected} bind:message={notFoundMessage} on:click={checkWord} on:drop={dragDrop} on:dragover={dragOver} on:dragenter={cardDragEnter} on:deselect={event => selectCard(event.detail)}/>
+	<Shelf
+		bind:currentWord={selected}
+		bind:message={notFoundMessage}
+		on:click={checkWord}
+		on:drop={dragDrop}
+		on:dragover={dragOver}
+		on:dragenter={cardDragEnter}
+		on:deselect={event => selectCard(event.detail)}
+	/>
 
 	<hr />
 
 	<div class="row">
 		{#each layout.columns as column, columnIdx}
-	<div class="column">
-		{#each column as c, i}
-			<Card face={c} turned={i < column.length - 1} stacked bind:selected={deck[c.deckPosition].selected} on:dblclick={_ => selectCard(c)} on:dragstart={e => startDrag(e, c, columnIdx)}  on:touchstart={e => handleTouchStart(e, c, columnIdx)} on:touchmove={handleTouchMove} on:touchend={handleTouchEnd}/>
-		{:else}
-			<Card />
+			<div class="column">
+				{#each column as c, i}
+					<Card
+						face={c}
+						turned={i < column.length - 1}
+						stacked
+						bind:selected={deck[c.deckPosition].selected}
+						on:dblclick={_ => selectCard(c)}
+						on:dragstart={e => startDrag(e, c, columnIdx)}
+						on:touchstart={e => handleTouchStart(e, c, columnIdx)}
+						on:touchmove={handleTouchMove}
+						on:touchend={handleTouchEnd}
+					/>
+				{:else}
+					<Card />
+				{/each}
+			</div>
 		{/each}
-	</div>
-	{/each}
 	</div>
 
 	<div class="discard">
 		{#if layout.discard.length > discardIndex + 3}
-		<Card face={genericCard} turned on:click={dealDiscard}/>
-			{:else}
-			<Card emptyText={layout.discard.length > 3 ? '🔄' : '❌'} on:click={dealDiscard}/>
-			{/if}
+			<Card face={genericCard} turned on:click={dealDiscard} />
+		{:else}
+			<Card
+				emptyText={layout.discard.length > 3 ? "🔄" : "❌"}
+				on:click={dealDiscard}
+			/>
+		{/if}
 		{#each layout.discard.slice(discardIndex, discardIndex + 3) as c}
-			<Card face={c} bind:selected={deck[c.deckPosition].selected} on:dblclick={_ => selectCard(c)} on:dragstart={e => startDrag(e, c, -1)} on:touchstart={e => handleTouchStart(e, c, -1)} on:touchmove={handleTouchMove} on:touchend={handleTouchEnd}/>
+			<Card
+				face={c}
+				bind:selected={deck[c.deckPosition].selected}
+				on:dblclick={_ => selectCard(c)}
+				on:dragstart={e => startDrag(e, c, -1)}
+				on:touchstart={e => handleTouchStart(e, c, -1)}
+				on:touchmove={handleTouchMove}
+				on:touchend={handleTouchEnd}
+			/>
 		{/each}
 		{#each Array(3 - layout.discard.slice(discardIndex, discardIndex + 3).length) as _}
 			<Card />
@@ -197,15 +292,17 @@ import {isWord, wordScore} from "./dictionary";
 	</div>
 
 	<div id="results">
-	{#if dragMessage && verbose}{dragMessage}{/if}
-	{#if notFound}<p id="notfound">{notFound} is not a word.</p>{/if}
-	<div id="found"><p id=score use:scrollIntoView >Score: {score}</p><ul>
-		{#each foundWords as w}
-		<li>{w}</li>
-		{/each}
-	</ul></div>
+		{#if dragMessage && verbose}{dragMessage}{/if}
+		{#if notFound}<p id="notfound">{notFound} is not a word.</p>{/if}
+		<div id="found">
+			<p id="score" use:scrollIntoView>Score: {score}</p>
+			<ul>
+				{#each foundWords as w}
+					<li>{w}</li>
+				{/each}
+			</ul>
+		</div>
 	</div>
-
 </main>
 
 <style>
@@ -214,8 +311,9 @@ import {isWord, wordScore} from "./dictionary";
 		padding: 1rem;
 		max-width: 95%;
 		margin: 0 auto;
-		font-size: 20vx;
-    font-family: "OpenDyslexic3", "OpenDyslexic2", "Dyslexie", "Comic Sans MS", cursive, sans-serif;
+		font-size: 1vw;
+		font-family: "OpenDyslexic3", "OpenDyslexic2", "Dyslexie", "Comic Sans MS",
+			cursive, sans-serif;
 	}
 
 	h1 {
